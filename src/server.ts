4994e891f04b9240dba0772cbb49ac6c5aecb6e0
Melvin Focke <79911517+melvinfocke@users.noreply.github.com>
2024-07-03 00:28:56 +0200

    Add CORS configuration

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,12 @@ export const MAIL_TO = process.env.MAIL_TO ?? '';
 server.set('trust proxy', true);
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 /* ROUTES */
 server.get('/dev-frontend', (req, res) => {
